83d53d6f23c223fbff2853326e16932459e2720c
Steve Fisher <steve.fisher@outlook.com>
2023-06-30 07:30:41 +0100

    updated server ip

--- bind9-terraform/main.tf
+++ bind9-terraform/main.tf
@@ -21,7 +21,7 @@ terraform {
 
 provider "dns" {
   update {
-    server        = "172.16.1.43"
+    server        = "172.16.1.2"
     key_name      = "tsig-key."
     key_algorithm = "hmac-sha256"
     key_secret    = var.tsig_key
